9f57d72bbaad88fc1c460c1207f40bd70c1811b4
Shukri Ali <shukr_ali@hotmail.com>
2018-12-09 01:12:30 +0000

    update the index.html page

--- js/script.js
+++ js/script.js
@@ -18,26 +18,27 @@ function hideUploadOption(){
   $("#profile-upload-option").css('display','none');
 }
 
-function removeProfilePhoto(){
-  hideUploadOption();
-  $("#userImage").val('');
-  $.ajax({
-    url: "remove.php",
-    type: "POST",
-    data:  new FormData(this),
-    beforeSend: function(){$("#body-overlay").show();},
-    contentType: false,
-    processData:false,
-    success: function(data)
-    {
-    $("#targetLayer").html('');
-    setInterval(function() {$("#body-overlay").hide(); },500);
-    },
-    error: function()
-    {
-    }
-  });
-}
+
+// function removeProfilePhoto(){
+//   hideUploadOption();
+//   $("#userImage").val('');
+//   $.ajax({
+//     url: "remove.php",
+//     type: "POST",
+//     data:  new FormData(this),
+//     beforeSend: function(){$("#body-overlay").show();},
+//     contentType: false,
+//     processData:false,
+//     success: function(data)
+//     {
+//     $("#targetLayer").html('');
+//     setInterval(function() {$("#body-overlay").hide(); },500);
+//     },
+//     error: function()
+//     {
+//     }
+//   });
+// }
 $(document).ready(function (e) {
   $("#uploadForm").on('submit',(function(e) {
     e.preventDefault();
@@ -50,12 +51,43 @@ $(document).ready(function (e) {
       processData:false,
       success: function(data)
       {
-      $("#targetLayer").css('opacity','1');
-      setInterval(function() {$("#body-overlay").hide(); },500);
+        $("#targetLayer").css('opacity','1');
+        setInterval(function() {$("#body-overlay").hide(); },500);
       },
       error: function()
       {
       }
-     });
+    });
   }));
 });
+
+
+$('#profileModal').on('click', '#btnProfile', function(){
+    var name = $('#nameInput').val();
+    var discr = $('#descrInput').val();
+    $('#indivP').val(name);
+    $('#indivName').val(discr);
+    $('#profileModal').modal('hide');
+});
+
+// $("body").on('click', '#btnProfile', function () {
+//     input = $(this).parent('td').prev('td').children('input[type=textbox]')
+//     var params = { id: $(this).attr('id'), value: input.val() };
+//     $.ajax({
+//         type: 'POST',
+//         url: '@Url.Action(SomeUrl- pointless)',
+//         dataType: 'json',
+//         cache: false,
+//         contentType: 'application/json; charset=utf­8',
+//         data: JSON.stringify(params),
+//         success: function (data, status) {
+//             doingsomething
+//         },
+//         error: function (er, info) {
+//
+//         },
+//         complete: function (xhr, status) {
+//
+//         },
+//     });
+// });
